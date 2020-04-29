b0ffc69abb958eeca789da0b55943d00b9841ccc
obvionaoe <35278323+obvionaoe@users.noreply.github.com>
2020-04-29 02:41:41 +0100

    removed old useless function

--- instagram/function.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('keydown', function(e) {
-  if (e.key === 'Enter') {
-    let a = document.querySelectorAll('button[type=button]').length;
-    ["Enviar", "Send", "Senden", "Invia", "Skicka", "Envoyer", "Verzenden", "Отправить"].includes(document.querySelectorAll('button[type=button]')[a-1].innerText) && document.querySelectorAll('button[type=button]')[a-1].click();
-  }
-});
