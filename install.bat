9d356a3bec4329798c2582966a4852c70a4fc051
obvionaoe <35278323+obvionaoe@users.noreply.github.com>
2020-07-20 00:35:43 +0100

    fixed windows script

--- install.bat
+++ install.bat
@@ -1,11 +1,49 @@
-echo off
+:: This is really bad but I don't really have time to learn batch right now, please forgive me xD
 
-if %1=="all" (
-  mklink "%AppData%\Franz\recipes\instagram\darkmode.css" "instagram\darkmode.css" && echo "Installed Instagram theme successfully"
-  mklink "%AppData%\Franz\recipes\messenger\darkmode.css" "messenger\darkmode.css" && echo "Installed Messenger theme successfully"
-  mklink "%AppData%\Franz\recipes\telegram\darkmode.css" "telegram\darkmode.css" && echo "Installed Telegram theme successfully"
-  mklink "%AppData%\Franz\recipes\whatsapp\darkmode.css" "whatsapp\darkmode.css" && echo "Installed WhatsApp theme successfully"
+if %1=="Ferdi" (
+  if %2=="all" (
+    mklink "%AppData%\Ferdi\recipes\instagram\darkmode.css" "instagram\darkmode.css" && echo "Installed Instagram theme successfully"
+    mklink "%AppData%\Ferdi\recipes\messenger\darkmode.css" "messenger\darkmode.css" && echo "Installed Messenger theme successfully"
+    mklink "%AppData%\Ferdi\recipes\telegram\darkmode.css" "telegram\darkmode.css" && echo "Installed Telegram theme successfully"
+    mklink "%AppData%\Ferdi\recipes\whatsapp\darkmode.css" "whatsapp\darkmode.css" && echo "Installed WhatsApp theme successfully"
+  )
+  else (
+    mklink "%AppData%\Ferdi\recipes\%2\darkmode.css" "%1\darkmode.css" && echo "Installed %2 theme successfully"
+  )
+)
 
-else (
-  mklink "%AppData%\Franz\recipes\%1\darkmode.css" "%1\darkmode.css" && echo "Installed %1 theme successfully"
+if %1=="Franz" (
+  if %2=="all" (
+    mklink "%AppData%\Franz\recipes\instagram\darkmode.css" "instagram\darkmode.css" && echo "Installed Instagram theme successfully"
+    mklink "%AppData%\Franz\recipes\messenger\darkmode.css" "messenger\darkmode.css" && echo "Installed Messenger theme successfully"
+    mklink "%AppData%\Franz\recipes\telegram\darkmode.css" "telegram\darkmode.css" && echo "Installed Telegram theme successfully"
+    mklink "%AppData%\Franz\recipes\whatsapp\darkmode.css" "whatsapp\darkmode.css" && echo "Installed WhatsApp theme successfully"
+  )
+  else (
+    mklink "%AppData%\Franz\recipes\%2\darkmode.css" "%1\darkmode.css" && echo "Installed %2 theme successfully"
+  )
+)
+
+if %1=="ferdi" (
+  if %2=="all" (
+    mklink "%AppData%\Ferdi\recipes\instagram\darkmode.css" "instagram\darkmode.css" && echo "Installed Instagram theme successfully"
+    mklink "%AppData%\Ferdi\recipes\messenger\darkmode.css" "messenger\darkmode.css" && echo "Installed Messenger theme successfully"
+    mklink "%AppData%\Ferdi\recipes\telegram\darkmode.css" "telegram\darkmode.css" && echo "Installed Telegram theme successfully"
+    mklink "%AppData%\Ferdi\recipes\whatsapp\darkmode.css" "whatsapp\darkmode.css" && echo "Installed WhatsApp theme successfully"
+  )
+  else (
+    mklink "%AppData%\Ferdi\recipes\%2\darkmode.css" "%1\darkmode.css" && echo "Installed %2 theme successfully"
+  )
+)
+
+if %1=="franz" (
+  if %2=="all" (
+    mklink "%AppData%\Franz\recipes\instagram\darkmode.css" "instagram\darkmode.css" && echo "Installed Instagram theme successfully"
+    mklink "%AppData%\Franz\recipes\messenger\darkmode.css" "messenger\darkmode.css" && echo "Installed Messenger theme successfully"
+    mklink "%AppData%\Franz\recipes\telegram\darkmode.css" "telegram\darkmode.css" && echo "Installed Telegram theme successfully"
+    mklink "%AppData%\Franz\recipes\whatsapp\darkmode.css" "whatsapp\darkmode.css" && echo "Installed WhatsApp theme successfully"
+  )
+  else (
+    mklink "%AppData%\Franz\recipes\%2\darkmode.css" "%1\darkmode.css" && echo "Installed %2 theme successfully"
+  )
 )
